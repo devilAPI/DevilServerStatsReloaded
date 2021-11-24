e74820566b058ce8bc5ea76a053fc1716d1b8ac6
Felix Seiboldt <52862870+devilAPI@users.noreply.github.com>
2021-11-24 23:39:55 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -4,4 +4,6 @@ require('dotenv').config({ path: '../.env' })
 module.exports.run = async (client, message, args) => {
   message.channel.send("!help - views this help");
   message.channel.send("!serverstats - activate/deactivate serverstats");
+  message.channel.send("!info - shows info");
+  message.channel.send("!invite - invite the bot");
 };
