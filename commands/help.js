b079f9afd6ca5bce77825c744f02e27f3e8b702d
Felix Seiboldt <52862870+devilAPI@users.noreply.github.com>
2021-12-10 12:31:51 +0100

    better help cmd

--- commands/help.js
+++ commands/help.js
@@ -2,8 +2,5 @@ const Discord = require("discord.js");
 require('dotenv').config({ path: '../.env' })
 
 module.exports.run = async (client, message, args) => {
-  message.channel.send("!help - views this help");
-  message.channel.send("!serverstats - activate/deactivate serverstats");
-  message.channel.send("!info - shows info");
-  message.channel.send("!invite - invite the bot");
+  message.channel.send(process.env.LANGUAGE_HELP_COMMAND);
 };
